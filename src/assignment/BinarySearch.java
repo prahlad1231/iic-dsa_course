8bbdc2e704271685b86861e8411908ae345c8e7b
prahlad1231 <prahlad1231@gmail.com>
2020-05-29 16:53:37 +0545

    content of fifth class

--- src/assignment/BinarySearch.java
+++ src/assignment/BinarySearch.java
@@ -8,6 +8,17 @@ public class BinarySearch {
     }
 
     public static void main(String[] args) {
-        String[] names = {"apple", "appzz", "ball", "cat", "dog"};
+        String[] names = {"apple", "appzz", "ball", "cat", "dog", "elephant", "fox", "giraffe",
+                        "joker", "kite", "lemon", "lion", "man", "mouse", "net", "nike", "orange",
+                        "poke", "zebra"};
+        BinarySearch binarySearch = new BinarySearch();
+        for (int i = 0; i < names.length; ++i) {
+            int index = binarySearch.binarySearch(names, 0, names.length, names[i]);
+            System.out.println("Index of " + names[i] + " is " + index);
+        }
+        int index = binarySearch.binarySearch(names, 0, names.length, "cow");
+        if (index == -1) {
+            System.out.println("Element not found!");
+        }
     }
 }
